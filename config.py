71de199faa496318760112f0c2ff2eb5c9fedff4
fernaned <fernandoed.bm@gmail.com>
2021-06-14 15:55:28 -0500

    updated procfile and config object

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY=os.getenv("SECRET_KEY") or "faiosdfjasdf"
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL").replace('postgres', 'postgresql') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL").replace('postgres', 'postgresql') if os.getenv("DATABASE_URL") else 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
